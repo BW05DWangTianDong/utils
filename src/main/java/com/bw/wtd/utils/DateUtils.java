cc4f8972c311a4078befb88de158c5e919b1f3a4
BW05DWangTianDong <2420159836@qq.com>
2019-09-06 14:14:48 +0800

    计算年龄工具

--- src/main/java/com/bw/wtd/utils/DateUtils.java
+++ src/main/java/com/bw/wtd/utils/DateUtils.java
@@ -1,5 +1,49 @@
 package com.bw.wtd.utils;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class DateUtils {
+	
+	/**
+	 * 
+	 * @Title:         getAge   
+	 * @Description:   TODO
+	 * @param:         @param Birthday
+	 * @param:         @return      
+	 * @return:        int     
+	 * @date:          2019年9月6日 下午2:06:29   
+	 * @throws
+	 */
+	public static int getAge(Date Birthday) {
+		
+		Calendar instance = Calendar.getInstance();
+		
+		instance.setTime(Birthday);
+		
+		int birthYear = instance.get(Calendar.YEAR);
+		
+		int birthMonth = instance.get(Calendar.MONTH);
+		
+		int birthDay = instance.get(Calendar.DAY_OF_MONTH);
+		
+		instance.setTime(new Date());
+		
+		int currentYear = instance.get(Calendar.YEAR);
+		
+		int currentMonth = instance.get(Calendar.MONTH);
+		
+		int currentDay = instance.get(Calendar.DAY_OF_MONTH);
+		
+		int age = currentYear-birthYear;
+		
+		if(currentMonth<birthMonth) {
+			age--;
+		}else if(currentMonth==birthMonth && currentDay<birthDay) {
+			age--;
+		}
+		
+		return age;
+	}
 
 }
